9b59f29531a4f0d87f99a51784e0c4f36461a6c2
Eavid <ganrinong@datagrand.com>
2024-02-01 17:41:34 +0800

    feat(bench_ui): 倍速显示

--- src/bench_ui/assets/components/speed_compare.tsx
+++ src/bench_ui/assets/components/speed_compare.tsx
@@ -1,6 +1,5 @@
-// @deno-types="https://esm.sh/echarts-comp@0.0.2"
 import { ECharts, EChartsOption } from "echarts-comp";
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 export type CompareItem = {
   name: string;
   time: number;
@@ -11,27 +10,61 @@ export function SpeedCompare(props: {
   chartType?: "line" | "bar";
   dimensions: string[];
   title: string;
+  yName?: string;
 }) {
-  const { source, dimensions, title, chartType = "line" } = props;
+  const { source, dimensions, title, chartType = "bar", yName } = props;
+  const [base, setBase] = useState<string | undefined>();
+
+  const ref = useRef<ECharts>(null);
   const compareChart = useMemo((): EChartsOption => {
     return {
-      title: { text: title },
+      title: { text: title, left: "center" },
       tooltip: { show: true, trigger: "axis" },
-      legend: { show: true },
+      legend: { show: true, top: "30px" },
       xAxis: {
         type: "category",
       },
       yAxis: {
         type: "value",
+        name: yName,
       },
       dataset: {
         dimensions: dimensions,
         source: source,
       },
-      series: dimensions.slice(1).map(() => ({ type: chartType })),
+      series: dimensions.slice(1).map((name) => {
+        return {
+          label: {
+            show: base ? name !== base : false,
+            formatter: (p: any) => {
+              const seriesName = p.seriesName;
+              if (!seriesName || !base) return "unknown";
+              const dataSetItem = source[p.dataIndex];
+              let baseValue = dataSetItem[base] as number;
+              let curentValue = dataSetItem[seriesName] as number;
+              let speed = baseValue / curentValue;
+
+              return speed.toFixed(2) + ` X`;
+            },
+            position: "top",
+          },
+          type: chartType,
+        };
+      }),
     };
-  }, [source, dimensions, chartType]);
-  return <ECharts option={compareChart} style={{ width: "100%", height: 400 }} />;
+  }, [source, dimensions, base]);
+  useEffect(() => {
+    ref.current!.on("click", (e) => {
+      const name = e.seriesName;
+      if (!name) return;
+      setBase((raw) => (raw === name ? undefined : name));
+    });
+  }, []);
+  return (
+    <div>
+      <ECharts ref={ref} option={compareChart} style={{ width: "100%", height: 400 }} />
+    </div>
+  );
 }
 
 export type DataSetItem = { [key: string]: number | string };
